0aad4e5fae755d6057bdf27c82657555c7059aec
Oleksandrozhoh <oleksandrozhoh@gmail.com>
2025-12-25 16:46:20 -0600

    filter by categories

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -17,15 +17,28 @@ const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
   const { projects } = loaderData as { projects: Project[] };
 
   const [currentPage, setCurrentPage] = useState(1);
+  const [filterCategory, setFilterCategory] = useState("All");
   const projectsPerPage = 6;
 
+  // catorories for filtering
+  const categories = [
+    "All",
+    ...new Set(projects.map((project) => project.category)),
+  ];
+
+  // filter projects by category
+  const filteredProjects =
+    filterCategory === "All"
+      ? projects
+      : projects.filter((project) => project.category === filterCategory);
+
   // calculate total pages
-  const totalPages = Math.ceil(projects.length / projectsPerPage);
+  const totalPages = Math.ceil(filteredProjects.length / projectsPerPage);
 
   // get current page projects
   const indexOfLastProject = currentPage * projectsPerPage;
   const indexOfFirstProject = indexOfLastProject - projectsPerPage;
-  const currentProjects = projects.slice(
+  const currentProjects = filteredProjects.slice(
     indexOfFirstProject,
     indexOfLastProject
   );
@@ -35,6 +48,24 @@ const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
       <h2 className="text-3xl text-white font-bold mb-8 text-center">
         💼 Projects
       </h2>
+      <div>
+        {categories.map((category) => (
+          <button
+            key={category}
+            onClick={() => {
+              setFilterCategory(category);
+              setCurrentPage(1); // reset to first page on filter change
+            }}
+            className={`mr-2 mb-4 px-4 py-2 rounded ${
+              filterCategory === category
+                ? "bg-blue-600 text-white"
+                : "bg-gray-700 text-gray-300 hover:bg-gray-600"
+            }`}
+          >
+            {category}
+          </button>
+        ))}
+      </div>
       <ProjectsList projects={currentProjects} />
       {
         <Pagination
